ddc86e173538c7a0319f4cb957f43d2f1ab88d84
Rakib Uddin <16103030@iubat.edu>
2019-02-17 14:24:17 +0600

    Added post route

--- Controllers/landing.js
+++ Controllers/landing.js
@@ -1,3 +1,8 @@
-exports.index = function(req, res, next) {
+exports.landing = function(req, res, next) {
     res.render('landing', { title: 'EXPRESS' });
   };
+
+exports.submit_lead = (req, res , next) => {
+    console.log(req.body.email_lead);
+      res.redirect("/")
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ let landing = require("../Controllers/landing")
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', landing.index)
+router.get('/', landing.landing)
+router.post("/", landing.submit_lead)
 
 module.exports = router;
