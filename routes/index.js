fb7aba4bf87507d27d69085ba043a9cdfdd49641
Rakib Uddin <16103030@iubat.edu>
2019-02-17 13:47:01 +0600

    Added Form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
+let landing = require("../Controllers/landing")
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', landing.index)
 
 module.exports = router;
